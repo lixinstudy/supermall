8a6d5663915e6aa8192eb56dcdf3031cbd24f162
lixin <zlx_safe@163.com>
2022-07-01 19:20:12 +0800

    基础构建

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  transpileDependencies: true
+  // transpileDependencies: ['swiper'],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
 }
